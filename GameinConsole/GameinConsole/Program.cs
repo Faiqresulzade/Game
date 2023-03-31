5c8e6f5897ade915b63489ab9c70d1988326a61b
Faiqresulzade <faigbr@code.edu.az>
2023-03-31 19:00:20 +0400

    added

---
+++ GameinConsole/GameinConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GameinConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word = { "Salam", "Faig", "resulzade", "Samir", "Manafov" };
+           
+            for (int i = 0; i < word.Length; i++)
+            {
+            Head:
+                for (int j = 0; j < word[i].Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write(word[i][j]);
+                    }
+                    if (j == word[i].Length - 1)
+                    {
+                        Console.Write(word[i][j]);
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                for (int j = 0; j < word[i].Length-2; j++)
+                {
+                     InputLetter:
+                    string letter = Console.ReadLine();
+                    if (letter == word[i][j+1].ToString())
+                    {
+                        Console.WriteLine("ela");
+                        j++;
+                        if (j== word[i].Length-2)
+                        {
+                            Console.WriteLine("Tebriklerrrr");
+                            i++;
+                            goto Head;
+                        }
+                        goto InputLetter;
+                    }
+                    if(letter != word[i][j + 1].ToString())
+                    {
+                        Console.WriteLine("Duzgun daxil edin!");
+                        goto InputLetter;
+                    }
+                }
+            }
+        }
+    }
+}
